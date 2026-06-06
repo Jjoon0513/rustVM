d1dd47ee230f3757cbc6fcbb942ef3251bd48d00
Jjoon0513 <ijjun244@gmail.com>
2026-06-06 23:05:35 +0900

    2026-06-06:1105
    Expand VM to 16 registers and make core fields public (registers, pc, memory, flags). Expose a pub new() and add memory helper methods (get_memory_u8, get_memory_u16) plus add_high_low (advances pc and composes u16). Refactor step() to use these helpers, update opcode range comments, and implement/add handlers for immediate/register add/sub variants (movi/movr, addi/addr, subi/subr). Add TODO for MUL opcodes and overflow flag handling.

--- src/vm.rs
+++ src/vm.rs
@@ -6,32 +6,54 @@ const SF: u8 = 1 << 4;
 const OF: u8 = 1 << 5;
 
 pub struct Vm {
-    registers: [u16; 8],
-    pc: usize,
-    memory: [u8; 65536],
-    flags: u8,
+    pub registers: [u16; 16],
+    pub pc: usize,
+    pub memory: [u8; 65536],
+    pub flags: u8,
 }
 
 impl Vm {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
-            registers: [0; 8],
+            registers: [0; 16],
             pc: 0,
             memory: [0; 65536],
             flags: 0,
         }
     }
-    fn step(&mut self) {
-        let opcode = self.memory[self.pc as usize];
+
+
+    pub fn get_memory_u8(&self) -> u8 {
+        self.memory[self.pc as usize]
+    }
+
+    pub fn get_memory_u16(&self) -> u16 {
+        self.memory[self.pc as usize] as u16
+    }
+
+    ///이 함수는 프로그램 카운터를 한칸 앞으로 옮김 `self.pc += 1`
+    ///
+    /// Advances the program counter by one. `self.pc += 1`
+    pub fn add_high_low(&mut self) -> u16 {
+        let low= self.get_memory_u16();
+        self.pc += 1;
+
+        let high = self.get_memory_u16();
+
+        low | (high << 8)
+    }
+
+    pub fn step(&mut self) {
+        let opcode = self.get_memory_u8();
         self.pc += 1;
 
         /*
 0x00 ~ 0x07	시스템
 0x08 ~ 0x0F	MOV
-0x10 ~ 0x17	ADD
-0x18 ~ 0x1F	SUB
-0x20 ~ 0x27	AND
-0x28 ~ 0x2F	OR/XOR
+0x10 ~ 0x17	Undecided
+0x18 ~ 0x1F	ADD/SUB
+0x20 ~ 0x27	MUL/DIV
+0x28 ~ 0x2F	AND/OR/XOR
 0x30 ~ 0x37	CMP
 0x38 ~ 0x3F	JMP
 0x40 ~ 0x47	PUSH/POP
@@ -45,26 +67,21 @@ impl Vm {
             0x00 => {} //nop
             0x08 => {
                 //movi: movi <Register> <LOWb> <HIGHb> (Lb + Hb -> R)
-                let reg = self.memory[self.pc];
+                let reg = self.get_memory_u8();
                 self.pc += 1;
 
-                let low = self.memory[self.pc] as u16;
+                let value = (&mut *self).add_high_low();
                 self.pc += 1;
-
-                let high = self.memory[self.pc] as u16;
-                self.pc += 1;
-
-                let value = low | (high << 8);
 
                 self.registers[reg as usize] = value;
             }
             0x09 => {
 
                 //movr: movr <Register0> <Register1> (R1 -> R0)
-                let reg0 = self.memory[self.pc];
+                let reg0 = self.get_memory_u8();
                 self.pc += 1;
 
-                let reg1 = self.memory[self.pc];
+                let reg1 = self.get_memory_u8();
                 self.pc += 1;
 
                 self.registers[reg0 as usize] = self.registers[reg1 as usize];
@@ -72,21 +89,51 @@ impl Vm {
 
 
 
-            0x10 => {
+            0x18 => {
                 //addi: addi <Register> <LOWb> <HIGHb>
-                let reg = self.memory[self.pc];
+                let reg = self.get_memory_u8();
                 self.pc += 1;
 
-                let low = self.memory[self.pc] as u16;
+                let val = (&mut *self).add_high_low();
                 self.pc += 1;
-
-                let high = self.memory[self.pc] as u16;
-                self.pc += 1;
-                
-
-                let val = low | (high << 8);
-                self.registers[reg as usize] = self.registers[reg as usize] + val;
+                self.registers[reg as usize] += val;
             }
+            0x19 => {
+                //addr: addr <Register0> <Register1>
+                let reg0 = self.get_memory_u8();
+                self.pc += 1;
+
+                let reg1 = self.get_memory_u8();
+                self.pc += 1;
+
+                self.registers[reg0 as usize] += self.registers[reg1 as usize];
+            }
+
+            0x1A => {
+                //subi: subi <Register> <LOWb> <HIGHb>
+                let reg = self.get_memory_u8();
+                self.pc += 1;
+
+                let val = (&mut *self).add_high_low();
+                self.pc += 1;
+                self.registers[reg as usize] -= val;
+            }
+            0x1B => {
+                //subr: subr <Register0> <Register1>
+                let reg0 = self.get_memory_u8();
+                self.pc += 1;
+
+                let reg1 = self.get_memory_u8();
+                self.pc += 1;
+
+                self.registers[reg0 as usize] -= self.registers[reg1 as usize];
+            }
+
+            /*
+            TODO
+            MULI, MULR (0x20, 0x21)
+            그리고 저 연산함수에서 레지스트리가 u16 오버플로면 오버플로 플래그도 옮기게 바꿔야함 :)
+             */
 
             _ => panic!("Unknown opcode"),
         }

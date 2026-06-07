0d92152e056bf3dcfc500d9adc383aeca1188100
Jjoon0513 <ijjun244@gmail.com>
2026-06-07 13:31:09 +0900

    Add flag helpers and update ALU ops
    Introduce set_flag/get_flag helpers and make ALU instructions update condition flags. add_high_low now advances PC by two and redundant PC increments were removed. addi/addr and subi/subr use overflowing_add/overflowing_sub to compute result and carry/borrow, set CF/ZF/SF/OF appropriately, and store the corrected result; operand parsing/variable names were clarified. These changes enable proper flag semantics for arithmetic operations and fix PC advancement bugs.

--- src/vm.rs
+++ src/vm.rs
@@ -22,6 +22,17 @@ impl Vm {
         }
     }
 
+    fn set_flag(&mut self, flag: u8, value: bool) {
+        if value {
+            self.flags |= flag;
+        } else {
+            self.flags &= !flag;
+        }
+    }
+
+    fn get_flag(&self, flag: u8) -> bool {
+        self.flags & flag != 0
+    }
 
     pub fn get_memory_u8(&self) -> u8 {
         self.memory[self.pc as usize]
@@ -31,14 +42,17 @@ impl Vm {
         self.memory[self.pc as usize] as u16
     }
 
-    ///이 함수는 프로그램 카운터를 한칸 앞으로 옮김 `self.pc += 1`
+
+
+    ///이 함수는 프로그램 카운터를 두칸 앞으로 옮김 `self.pc += 2`
     ///
-    /// Advances the program counter by one. `self.pc += 1`
+    /// Advances the program counter by two. `self.pc += 2`
     pub fn add_high_low(&mut self) -> u16 {
         let low= self.get_memory_u16();
         self.pc += 1;
 
         let high = self.get_memory_u16();
+        self.pc += 1;
 
         low | (high << 8)
     }
@@ -71,7 +85,6 @@ impl Vm {
                 self.pc += 1;
 
                 let value = (&mut *self).add_high_low();
-                self.pc += 1;
 
                 self.registers[reg as usize] = value;
             }
@@ -90,43 +103,83 @@ impl Vm {
 
 
             0x18 => {
-                //addi: addi <Register> <LOWb> <HIGHb>
+                //addi: addi <Register> <LOWb> <HIGHb> (R += Lb + Hb)
                 let reg = self.get_memory_u8();
                 self.pc += 1;
 
                 let val = (&mut *self).add_high_low();
-                self.pc += 1;
-                self.registers[reg as usize] += val;
+
+                let rev = self.registers[reg as usize];
+
+                let (rst, carry) = rev.overflowing_add(val);
+
+                self.set_flag(CF, carry);
+                self.set_flag(ZF, rst == 0);
+                self.set_flag(SF, rst & 0x8000 != 0);
+                self.set_flag(OF, ((rev ^ rst) & (val ^ rst) & 0x8000) != 0);
+
+                self.registers[reg as usize] = rst;
             }
             0x19 => {
-                //addr: addr <Register0> <Register1>
-                let reg0 = self.get_memory_u8();
+                //addr: addr <Register0> <Register1> (R0 += R1)
+                let reg = self.get_memory_u8();
                 self.pc += 1;
 
-                let reg1 = self.get_memory_u8();
+                let val = self.get_memory_u8();
                 self.pc += 1;
 
-                self.registers[reg0 as usize] += self.registers[reg1 as usize];
+                let rev = self.registers[reg as usize];
+
+                let vav = self.registers[val as usize];
+
+                let (rst, carry) = rev.overflowing_add(vav);
+
+                self.set_flag(CF, carry);
+                self.set_flag(ZF, rst == 0);
+                self.set_flag(SF, rst & 0x8000 != 0);
+                self.set_flag(OF, ((rev ^ rst) & (vav ^ rst) & 0x8000) != 0);
+
+                self.registers[reg as usize] = rst;
             }
 
             0x1A => {
-                //subi: subi <Register> <LOWb> <HIGHb>
+                //subi: subi <Register> <LOWb> <HIGHb> (R -= Lb + Hb)
                 let reg = self.get_memory_u8();
                 self.pc += 1;
 
                 let val = (&mut *self).add_high_low();
-                self.pc += 1;
-                self.registers[reg as usize] -= val;
+
+                let rev = self.registers[reg as usize];
+
+                let (rst, borrow) = rev.overflowing_sub(val);
+
+                self.set_flag(CF, borrow);
+                self.set_flag(ZF, rst == 0);
+                self.set_flag(SF, rst & 0x8000 != 0);
+                self.set_flag(OF, ((rev ^ val) & (rev ^ rst) & 0x8000) != 0);
+
+                self.registers[reg as usize] = rst;
             }
             0x1B => {
-                //subr: subr <Register0> <Register1>
-                let reg0 = self.get_memory_u8();
+                //subr: subr <Register0> <Register1> (R0 -= R1)
+                let reg = self.get_memory_u8();
                 self.pc += 1;
 
-                let reg1 = self.get_memory_u8();
+                let val = self.get_memory_u8();
                 self.pc += 1;
 
-                self.registers[reg0 as usize] -= self.registers[reg1 as usize];
+                let rev = self.registers[reg as usize];
+
+                let vav = self.registers[val as usize];
+
+                let (rst, borrow) = rev.overflowing_sub(vav);
+
+                self.set_flag(CF, borrow);
+                self.set_flag(ZF, rst == 0);
+                self.set_flag(SF, rst & 0x8000 != 0);
+                self.set_flag(OF, ((rev ^ vav) & (rev ^ rst) & 0x8000) != 0);
+
+                self.registers[reg as usize] = rst;
             }
 
             /*

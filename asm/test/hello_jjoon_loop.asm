56e0067dcc2d088b82ebec195178d860ac6817d9
쭌이Jjoon <82186614+Jjoon0513@users.noreply.github.com>
2026-07-22 15:23:58 +0900

    Clarify syscalls with comments in hello_jjoon_loop.asm
    Added comments to clarify syscall instructions.

--- asm/test/hello_jjoon_loop.asm
+++ asm/test/hello_jjoon_loop.asm
@@ -5,14 +5,14 @@ movi r2, 14
 movi r1, msg
 
 write_loop:
-    syscall
+    syscall ; sys_write
     cmp r5, r4
     je write_exit
     subi r4, 1
     jmp write_loop
 write_exit:
     movi r0, 5
-    syscall
+    syscall ; sys_halt
 
 msg:
     db 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x4A, 0x6A, 0x6F, 0x6F, 0x6E, 0x21, 0x0A
